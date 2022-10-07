7522888e38c03d3c6ba7e82c8b6d37fc2ea38df2
Erik Scofield <erik@clearfunction.com>
2022-10-07 11:49:55 -0500

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { worker } from './mock/browser';
 
 var bob = import.meta.env.VITE_Mock;
 
 console.log(bob)
 
- const { worker } = await import('./mock/browser');
  worker.start();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
