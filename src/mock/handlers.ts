b00f8d0ae36bf3ca687fbea3209b807547f0526a
Erik Scofield <erik@clearfunction.com>
2022-10-13 08:53:06 -0500

    css

--- src/mock/handlers.ts
+++ src/mock/handlers.ts
@@ -4,12 +4,14 @@ import { Movie, Movies } from "../services/types";
 const baseURL = `https://mock-my.api.net/v1/`;
 
 export const handlers = [
+    /**** MOVIES GET HANDLER */
     rest.get(`${baseURL}movies`,(_: any, res: any, ctx: any) => {
       return res(
         ctx.status(200),
         ctx.json(movies),
       )
     }),
+    /**** Movie Post Handler */
     rest.post(`${baseURL}movie/:movieId`, (req: any, res: any, ctx: any) => {
       const { movieId } = req.params;
 
@@ -27,7 +29,7 @@ const movieList = [
     title: "The Shawshank Redemption",
     imageUrl: "https://i.ebayimg.com/images/g/McUAAOSw~e5ZW~ee/s-l400.jpg",
     description: "Two imprisoned"
-  } as Movie,
+  },
   {
     id: 8,
     title: "Safe At Home",
@@ -40,13 +42,12 @@ const movieList = [
     title: "Spiderman",
     imageUrl: "https://imgc.allpostersimages.com/img/posters/the-amazing-spider-man-movie-poster_u-L-F5UQ980.jpg?artHeight=550&artPerspective=n&artWidth=550",
     description: "Spiderman"
-  } as Movie,
+  },
   {
     id: 3,
     title: "Harry Potter",
     imageUrl: "https://i.ebayimg.com/images/g/HP0AAOSwKIpV-vwL/s-l500.jpg",
     description: "Harry Potter",
-    actors: []
   },
   {
     id: 7,
@@ -60,21 +61,18 @@ const movieList = [
     title: "Captain Marvel",
     imageUrl: "https://akns-images.eonline.com/eol_images/Entire_Site/2018112/rs_634x939-181202195654-634.captain-marvel.12418.jpg",
     description: "Captain Marvel",
-    actors: []
   },
   {
     id: 5,
     title: "Matrix",
     imageUrl: "https://www.themoviedb.org/t/p/original/vybQQ7w7vGvF53IsGD0y0JSgIsA.jpg",
     description: "Matrix",
-    actors: []
   },
   {
     id: 6,
     title: "The Incredibles",
     imageUrl: "https://i.ebayimg.com/images/g/3S8AAOSwAQpfjVRI/s-l500.jpg",
     description: "The Incredibles",
-    actors: []
   },
   {
     id: 9,
